d2c3849216f8524ec6d29725772f1f8c8800006d
adam nethaway <adamwnethaway@gmail.com>
2024-03-09 03:02:38 -0600

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'Adam',
+        'title': "Blog Post 1",
+        'content': "First post content",
+        'date_posted': 'March 9, 2024'
+    },
+    {
+        'author': 'John',
+        'title': "Blog Post 2",
+        'content': "Second post content",
+        'date_posted': 'March 9, 2024'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
+
